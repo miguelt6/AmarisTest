c8bb7cf23d47763af93fd46b5c92314cf9a437fc
miguelt6 <miguel.t6@gmail.com>
2017-12-07 20:36:41 +0000

    Add project files.

---
+++ Amaris/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Amaris.MvcApplication" Language="C#" %>
---
+++ Amaris/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Amaris.Startup))]
+namespace Amaris
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
